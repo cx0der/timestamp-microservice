5656546277c8f0c13326e222dc6491b63635f8e4
cx0der <277487+cx0der@users.noreply.github.com>
2018-12-02 18:45:55 +0530

    Implementation of the timestamp microservice
    Updated npm name and git url.
    Update versions of node engine, express and cors versions.
    Implementation of timestamp microservice

--- server.js
+++ server.js
@@ -19,8 +19,23 @@ app.get("/", function (req, res) {
 });
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/timestamp/:date_string", function (req, res) {
+  let input = req.params.date_string;
+  let val = Date.parse(input);
+  let output;
+  let t;
+  if(isNaN(val)) {
+    t = new Date();
+    t.setTime(input);
+  } else {
+    t = new Date();
+    t.setTime(val);
+  }
+  output = {
+    unix: t.getTime(),
+    utc: t.toUTCString()
+  };
+  res.json(output);
 });
 
 // listen for requests :)
